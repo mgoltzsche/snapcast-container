b69692862c3614acef23c8105746619d4710a3bf
Max Goltzsche <max.goltzsche@gmail.com>
2022-11-26 02:02:19 +0100

    fix: snapfifo creation fix

--- snapserver.sh
+++ snapserver.sh
@@ -7,7 +7,7 @@ envsubst "$(set | grep -Eo '^SNAPSERVER_[^=]+' | sed 's/^/$/')" < /etc/snapserve
 
 if [ "$SNAPSERVER_SOURCE_CREATE_FIFO" ]; then
 	echo "Creating snapserver source fifo at $SNAPSERVER_SOURCE_CREATE_FIFO"
-	[ "$SNAPSERVER_SOURCE_CREATE_FIFO" ] || -f mkfifo -m 640 "$SNAPSERVER_SOURCE_CREATE_FIFO"
+	[ -p "$SNAPSERVER_SOURCE_CREATE_FIFO" ] || mkfifo -m 640 "$SNAPSERVER_SOURCE_CREATE_FIFO"
 	echo Generating a bit noise to initialize snapserver
 	timeout 1 sh -c 'cat /dev/urandom > "$SNAPSERVER_SOURCE_CREATE_FIFO"' || true
 fi
