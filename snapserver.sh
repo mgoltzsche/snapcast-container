f7ca6fc67c3f348ec50aeec3a1a68931d7b27e5b
Max Goltzsche <max.goltzsche@gmail.com>
2022-12-04 06:33:02 +0100

    fix: let snap server stream sound 3s after start

--- snapserver.sh
+++ snapserver.sh
@@ -10,14 +10,22 @@ if [ "$SNAPSERVER_SOURCE_CREATE_FIFO" ]; then
 	[ -p "$SNAPSERVER_SOURCE_CREATE_FIFO" ] || mkfifo -m 640 "$SNAPSERVER_SOURCE_CREATE_FIFO"
 	echo Generating a bit noise to initialize snapserver
 	if [ "$SNAPSERVER_SOUND_TEST" = true ]; then
-		sox -V -r 48000 -n -b 16 -c 2 /tmp/testsound1.wav synth 30 sin 0+19000 sin 1000+20000 vol -10db remix 1,2 channels 2
-		sox -V -r 48000 -n -b 16 -c 2 /tmp/testsound2.wav synth 3 sin 0+15000 sin 1000+80000 vol -10db remix 1,2 channels 2
+		sox -V -r 48000 -n -b 16 -c 2 /tmp/testsound.wav synth 30 sin 0+19000 sin 1000+20000 vol -10db remix 1,2 channels 2
 		while true; do
-			cat /tmp/testsound1.wav > "$SNAPSERVER_SOURCE_CREATE_FIFO"
-			cat /tmp/testsound2.wav > "$SNAPSERVER_SOURCE_CREATE_FIFO"
-			sleep 1
+			cat /tmp/testsound.wav > "$SNAPSERVER_SOURCE_CREATE_FIFO"
+			sleep 20
 		done &
 	fi
+fi
+
+if [ "$SNAPSERVER_START_SOUND_ENABLED" ] && echo "$SNAPSERVER_SOURCE" | grep -Eq '^pipe://'; then
+	(
+		sleep 3
+		echo Playing start sound
+		FIFO="$(echo "$SNAPSERVER_SOURCE" | sed -E 's!^pipe://!!; s!\?.*$!!')" &&
+		sox -V -r 48000 -n -b 16 -c 2 /tmp/start.wav synth 3 sin 0+15000 sin 1000+80000 vol -10db remix 1,2 channels 2 &&
+		cat /tmp/start.wav > "$FIFO"
+	) &
 fi
 
 set -x
